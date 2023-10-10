aa28a4d0451fbce0469e22858436dd244ea124bd
Filip14564654 <118005701+Filip14564654@users.noreply.github.com>
2023-10-10 11:41:20 +0200

    Initial commit

---
+++ src/main/java/com/interstellar/Interstellar/InterstellarApplication.java
@@ -0,0 +1,13 @@
+package com.interstellar.Interstellar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterstellarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterstellarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interstellar/Interstellar/InterstellarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interstellar.Interstellar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterstellarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
